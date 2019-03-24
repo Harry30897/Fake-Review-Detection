9c26cc29e666a07385a2d212b261867bcd062fd3
bharat.suri <bsuri97@gmail.com>
2019-03-24 17:03:31 +0530

    Added reviewer features

--- extract_features.py
+++ extract_features.py
@@ -44,6 +44,24 @@ def count_first_pronouns(review):
     return count
 
 
+def create_reviewer_dict(filename):
+    reviewers = {}
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.split(' ')
+            if line[2] not in reviewers.keys():
+                reviewers[line[2]] = {'count': 1}
+                reviewers[line[2]][line[0]] = line[8]
+            else:
+                reviewers[line[2]]['count'] += 1
+                if line[0] not in reviewers[line[2]].keys():
+                    reviewers[line[2]][line[0]] = line[8]
+                else:
+                    if int(line[8]) > int(reviewers[line[2]][line[0]]):
+                        reviewers[line[2]][line[0]] = line[8]
+    return reviewers
+
+
 def main():
     content_file, meta_file = sys.argv[1], sys.argv[2]
     columns = [
@@ -55,12 +73,17 @@ def main():
         'c_words',
         'f_pronouns',
         'rating',
+        # 'word_num_avg',
+        'total_reviews',
+        # 'rating_var',
+        'max_rating',
         'label'
     ]
     features = pd.DataFrame(
         columns=columns
     )
     count = 0
+    reviewer_dict = create_reviewer_dict(meta_file)
     with open(content_file, 'r') as reviews, open(meta_file, 'r') as meta_data:
         rows = []
         for review, meta in zip(reviews, meta_data):
@@ -76,6 +99,10 @@ def main():
             new_row['c_words'] = capital_words(review)
             new_row['f_pronouns'] = count_first_pronouns(review)
             new_row['rating'] = meta[8]
+            # new_row['word_num_avg'] = reviewer_dict[meta[2]][0]
+            new_row['total_reviews'] = reviewer_dict[meta[2]]['count']
+            # new_row['rating_var'] = reviewer_dict[meta[2]][0]
+            new_row['max_rating'] = reviewer_dict[meta[2]][meta[0]]
             new_row['label'] = meta[4]
             rows.append(new_row)
             count += 1
