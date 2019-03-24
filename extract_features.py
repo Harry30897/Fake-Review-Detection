026766e1f80672cd634810cb3825d7a0a6cda4e7
bharat.suri <bsuri97@gmail.com>
2019-03-24 15:10:36 +0530

    Added features in feature map

--- extract_features.py
+++ extract_features.py
@@ -1,5 +1,6 @@
 import sys
 import csv
+from datetime import datetime as dt
 from math import ceil
 import pandas as pd
 
@@ -44,25 +45,40 @@ def count_first_pronouns(review):
 
 
 def main():
-    file_name = sys.argv[1]
-    df = pd.DataFrame(
-        columns=['Length', 'C_Letters', 'C_Words', 'F_Pronouns']
+    content_file, meta_file = sys.argv[1], sys.argv[2]
+    columns = [
+        'reviewID',
+        'reviewerID',
+        'length',
+        'c_letters',
+        'c_words',
+        'f_pronouns',
+        'rating',
+        'label'
+    ]
+    features = pd.DataFrame(
+        columns=columns
     )
     count = 0
-    with open(file_name, 'r') as file:
+    with open(content_file, 'r') as reviews, open(meta_file, 'r') as meta_data:
         rows = []
-        # for line in csv.reader(file, dialect='excel-tab'):
-        for line in file:
+        for review, meta in zip(reviews, meta_data):
             new_row = {}
-            new_row['Length'] = total_length(line)
-            new_row['C_Letters'] = capital_letters(line)
-            new_row['C_Words'] = capital_words(line)
-            new_row['F_Pronouns'] = count_first_pronouns(line)
+            meta = meta.split(' ')
+            new_row['reviewID'] = meta[1]
+            new_row['reviewerID'] = meta[2]
+            new_row['length'] = total_length(review)
+            new_row['c_letters'] = capital_letters(review)
+            new_row['c_words'] = capital_words(review)
+            new_row['f_pronouns'] = count_first_pronouns(review)
+            new_row['rating'] = meta[8]
+            new_row['label'] = meta[4]
             rows.append(new_row)
             count += 1
             print('Extracted: {}'.format(count), end='\r')
-    df = df.append(rows, ignore_index=True)
-    df.to_csv('features.csv')
+    features = features.append(rows, ignore_index=True)
+    features = features.set_index('reviewID')
+    features.to_csv('features.csv')
 
 
 if __name__ == '__main__':
