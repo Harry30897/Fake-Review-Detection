8f193ac9b5f50580468080d1d8f217905980f30f
bharat.suri <bsuri97@gmail.com>
2019-03-25 14:00:42 +0530

    Training script

--- train.py
+++ train.py
@@ -1,22 +1,34 @@
 import pandas as pd
-features = pd.read_csv('features.csv', index_col='reviewID')
 from sklearn.model_selection import train_test_split
-len(features[features['label'] == 'Y'])
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
+from sklearn import svm
+from sklearn import metrics
+
+
+features = pd.read_csv('features.csv', index_col='reviewID')
 genuine = features[features['label'] == 'N']
 fake = features[features['label'] == 'Y']
 genuine = train_test_split(genuine, train_size=0.2, test_size=0.0)[0]
 sample = genuine.append(fake)
 data = sample.loc[:, 'length':'max_rating']
 labels = sample['label']
-x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.1)
-from sklearn.linear_model import LogisticRegression
+x_train, x_test, y_train, y_test = train_test_split(data,
+                                                    labels,
+                                                    test_size=0.1)
 logisticRegr = LogisticRegression()
 logisticRegr.fit(x_train, y_train)
 score = logisticRegr.score(x_test, y_test)
-print(score)
-from sklearn import svm
-clf = svm.SVC(kernel='rbf')
-clf.fit(x_train,y_train)
+print('Logistic Score: ', score)
+
+
+sgd = SGDClassifier(max_iter=1000)
+sgd.fit(x_train, y_train)
+score = sgd.score(x_test, y_test)
+print('SGD Score: ', score)
+
+
+clf = svm.SVC()
+clf.fit(x_train, y_train)
 y_pred = clf.predict(x_test)
-from sklearn import metrics
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print('SVM Score: ', metrics.accuracy_score(y_test, y_pred))
