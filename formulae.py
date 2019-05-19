f411b201811eb8a377810e9ef63e11202113e639
Hardik <hnangia3097@gmail.com>
2019-05-19 19:10:31 +0530

    Updated formulae script

--- formulae.py
+++ formulae.py
@@ -2,10 +2,56 @@ import sys
 
 meta_file = sys.argv[1]
 user = {}
-product = {}
 
 
-def user_trust_rating():
+def product_count():
+    product = {}
+    with open(meta_file, 'r') as mfile:
+        for line in mfile:
+            line = line.split(" ")
+            if line[3] not in product.keys():
+                product[line[3]] = {'count' : 1}
+            else:
+                product[line[3]]['count'] += 1
+    return product
+
+def create_reviewer_dict(filename):
+    reviewers = {}
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.split(' ')
+            rating = int(line[8])
+            if line[2] not in reviewers.keys():
+                reviewers[line[2]] = {'count': 1}
+                reviewers[line[2]][line[0]] = rating
+                if rating > 3:
+                    reviewers[line[2]]['positive'] = 1
+                    reviewers[line[2]]['negative'] = 0
+                else:
+                    reviewers[line[2]]['positive'] = 0
+                    reviewers[line[2]]['negative'] = 1
+                if line[4] == 'N':
+                    reviewers[line[2]]['genuine'] = 1
+                else:
+                    reviewers[line[2]]['genuine'] = 0
+            else:
+                reviewers[line[2]]['count'] += 1
+                if rating > 3:
+                    reviewers[line[2]]['positive'] += 1
+                else:
+                    reviewers[line[2]]['negative'] += 1
+                if line[4] == 'N':
+                    reviewers[line[2]]['genuine'] += 1
+                if line[0] not in reviewers[line[2]].keys():
+                    reviewers[line[2]][line[0]] = int(line[8])
+                else:
+                    if int(line[8]) > int(reviewers[line[2]][line[0]]):
+                        reviewers[line[2]][line[0]] = int(line[8])
+    return reviewers
+
+
+
+"""def user_trust_rating():
     with open(meta_file, 'r') as mfile:
         for line in mfile:
             line = line.split(" ")
@@ -20,7 +66,8 @@ def user_trust_rating():
             print(user)        
             user[line[2]]['trust_rating'] = user[line[2]]['tcount'] / user[line[2]]['count']
             print(user[line[2]]['trust_rating'])
-            break
+            break"""
+
 
 
 def goodness_of_restaurant():
@@ -31,7 +78,10 @@ def goodness_of_restaurant():
                 product[line[3]]['summison'] = line[8] * reviewer_dict[row[line[2]]]['genuine'] / row['total_reviews']
             else:
                 product[line[3]]['summison'] += line[8] * reviewer_dict[row[line[2]]]['genuine'] / row['total_reviews']
-            product[line[3]]['goodness'] = product[line[3]]['summison'] / 100
+            product[line[3]]['goodness'] = product[line[3]]['summison'] / product[line[3]]['count']
             break
-
 user_trust_rating()
+goodness_of_restaurant()
+
+
+
